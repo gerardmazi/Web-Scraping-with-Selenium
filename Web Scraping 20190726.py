29422426015ebe5cd54726af2bed378adc04c2e4
Gerard Mazi <gerard.mazi@gmail.com>
2019-07-27 14:48:14 -0700

    Update Web Scraping 20190726.py

--- Web Scraping 20190726.py
+++ Web Scraping 20190726.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri Jul 26 15:53:20 2019
 
-@author: gmazi
+@author: Gerard Mazi
+@email: gerard.mazi@gmail.com
+@phone: 862.221.2477
 """
 
 
@@ -12,7 +13,7 @@ import time
 from datetime import date
 import matplotlib.pyplot as plt
 
-chrome_path = r"K:/Treasury/CORPORATE TREASURY/Weekly Retail Pricing Package/Deposit Analytics/Deposit Rates/chromedriver.exe"
+chrome_path = r"C:/.../chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 
 url = ['https://www.depositaccounts.com/cd/3-month-cd-rates.html',
@@ -66,44 +67,5 @@ for u in lookup['URL'].tolist():
 nu_rates["Date"] = date.today()
 nu_rates = pd.merge(nu_rates, lookup, on='URL',how='left')
 nu_rates['Rate'] = nu_rates['Rate'].str[:4].astype('float')
-nu_rates = nu_rates[nu_rates.Bank != 'HomeStreet Bank']
 
-# Load Deposit Rates and apend new data
-dep = pd.read_pickle('K:/Treasury/CORPORATE TREASURY/Weekly Retail Pricing Package/Deposit Analytics/Deposit Rates/dep.pkl')
-dep = pd.concat([dep, nu_rates], ignore_index = True)
-
-# Add up to date HSB deposit data
-hsb_date = date(2019, 7, 23)
-hsb_rates = [('HomeStreet Bank', '3 month cd',  0.40, '', hsb_date, '03 Month'),
-             ('HomeStreet Bank', '7 month cd',  2.00, '', hsb_date, '06 Month'),
-             ('HomeStreet Bank', '11 month cd', 2.10, '', hsb_date, '12 Month'),
-             ('HomeStreet Bank', '13 month cd', 2.20, '', hsb_date, '12 Month'),
-             ('HomeStreet Bank', '15 month cd', 2.25, '', hsb_date, '12 Month'),
-             ('HomeStreet Bank', '18 month cd', 2.30, '', hsb_date, '18 Month'),
-             ('HomeStreet Bank', '22 month cd', 2.35, '', hsb_date, '24 Month'),
-             ('HomeStreet Bank', '36 month cd', 2.40, '', hsb_date, '36 Month'),
-             ('HomeStreet Bank', '60 month cd', 2.40, '', hsb_date, '60 Month'),
-             ('HomeStreet Bank', 'Savings',     0.20, '', hsb_date, 'Savings' ),
-             ('HomeStreet Bank', 'PPMMS',       2.25, '', hsb_date, 'Money Market')]
-hsb_data = pd.DataFrame(hsb_rates, columns = list(dep))
-
-# Append and save
-dep = pd.concat([dep, hsb_data], ignore_index = True)
-dep.to_pickle('K:/Treasury/CORPORATE TREASURY/Weekly Retail Pricing Package/Deposit Analytics/Deposit Rates/dep.pkl')
-
-#########################################################################################
-# ANALYTICS AND TRENDS
-
-hsb_trend = dep[dep.Bank=='HomeStreet Bank'].groupby(['Date','Product'])['Rate'].mean().unstack()
-
-means = dep.groupby(['Date','Product']).mean().unstack()
-means.plot()
-
-median = dep.groupby(['Date','Product']).median().unstack()
-median.plot()
-
-quantile = dep.groupby(['Date','Product']).quantile(0.99).unstack()
-quantile.plot()
-
-max_rate = dep.groupby(['Date','Product'])['Rate'].max().unstack()
-max_rate.plot()
+# Load a running file to which you append new data
